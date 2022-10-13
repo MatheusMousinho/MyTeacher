bdfea6a36727dbf5e8303fe113607120491f4de4
Matheus Mousinho <wznmatheus@gmail.com>
2022-10-13 12:21:21 -0300

    first commit

---
+++ backend/app/views.py
@@ -0,0 +1,7 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HomeApiView(APIView):
+    def get(self, request, format=None):
+        return Response({"nome": "Matheus Mousinho" , "idade" : 22}, status=200)
+
